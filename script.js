38cc2550f56ece6091846923f112af7520611055
lbastos17 <bastosls17@gmail.com>
2024-01-30 19:26:35 -0300

    first* , This project is ongoing

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    const result = Math.floor(Math.random() * (max - min + 1)) + min; 
     
     alert(result)
 } 
