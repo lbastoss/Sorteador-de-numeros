54f6c587bf5e160c466855d3e051602f3dafa6b4
lbastos17 <bastosls17@gmail.com>
2024-02-01 16:52:48 -0300

    adding alert about values min e max

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min; 
-    
-    alert(result)
+    if (min >= max) {
+        alert('O valor mínimo tem que ser MENOR do que o valor máximo')
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        alert(result)
+    }
+
+
 } 
